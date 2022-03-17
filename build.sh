97393bb536a5bbc0f0ac8e89f35db84246b82123
Márton Boros <martonboros@gmail.com>
2022-03-17 10:52:48 +0200

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-# exec idris2 --build responsible.ipkg
-exec idris2 --codegen node --build responsible.ipkg
+exec idris2 --build responsible.ipkg
+# exec idris2 --codegen node --build responsible.ipkg
